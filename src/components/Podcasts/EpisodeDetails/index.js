cd3bff230bfe58a195c001cfec893902e345c01a
= <=>
2023-08-23 19:53:07 +0530

    update

--- src/components/Podcasts/EpisodeDetails/index.js
+++ src/components/Podcasts/EpisodeDetails/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Button from "../../common/Button";
+import "./styles.css";
 
 function EpisodeDetails({ index, title, description, audioFile, onClick }) {
   return (
     <div style={{ width: "100%" }}>
-      <h1 style={{ textAlign: "left", marginBottom: 0 }}>
+      <h1 className="episode-title" style={{ textAlign: "left", marginBottom: 0 }}>
         {index}. {title}
       </h1>
       <p style={{ marginLeft: "1.5rem" }} className="podcast-description ">
