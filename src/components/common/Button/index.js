c57645b11aa29d3b2406dd0bfde08e59295272cc
= <=>
2023-08-23 16:56:48 +0530

    update

--- src/components/common/Button/index.js
+++ src/components/common/Button/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./styles.css";
+import Image from "../../../assets/logo.png";
 function Button({ text, onClick, disabled, width, isGoogleEnabled }) {
   return (
     <div
       onClick={onClick}
-      className="custom-btn"
+      className={isGoogleEnabled ? "google-btn" : "custom-btn"}
       disabled={disabled}
       style={{ width: width }}
     >
+      {isGoogleEnabled && <img className="google-icon" src={Image} />}
       {text}
     </div>
   );
